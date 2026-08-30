c19a8c4999299f90d9d14c1f8665c5d5ad4b7ed8
Clara Kistner <clarabkistner@gmail.com>
2026-08-30 11:04:17 -0300

    release: pasta do spring initializr

---
+++ eixo/src/main/java/com/example/eixo/EixoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eixo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EixoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EixoApplication.class, args);
+	}
+
+}
---
+++ eixo/src/test/java/com/example/eixo/EixoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eixo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EixoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
