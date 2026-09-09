ccadf549bf6c58478865d52f57b426665cc30af7
vinipxg06-pixel <vinipxg06@gmail.com!>
2026-09-09 19:30:30 -0300

    feature: oficina - MVC

--- eixo/src/main/java/com/example/eixo/oficina/controller/OficinaController.java
+++ eixo/src/main/java/com/example/eixo/oficina/controller/OficinaController.java
@@ -1,4 +1,39 @@
-package com.example.eixo.oficina;
+package com.example.eixo.oficina.controller;
 
-public class controller {
+import com.example.eixo.oficina.model.Oficina;
+import com.example.eixo.oficina.service.OficinaService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.boot.jaxb.mapping.spi.JaxbFetchProfileImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/oficinas")
+@RequiredArgsConstructor
+public class OficinaController {
+
+    private final OficinaService oficinaService;
+
+    @GetMapping
+    public List<Oficina> listar(){
+      return oficinaService.listar();
+    };
+
+    @GetMapping("/{id}")
+    public Oficina buscarId(Long id){
+        return oficinaService.buscarId(id);
+    };
+
+    @PostMapping
+    public Oficina salvar(@RequestBody Oficina oficina) {
+        return oficinaService.salvar(oficina);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        oficinaService.deletar(id);
+    }
+
+
 }
--- eixo/src/main/java/com/example/eixo/oficina/repository/OficinaRepository.java
+++ eixo/src/main/java/com/example/eixo/oficina/repository/OficinaRepository.java
@@ -1,4 +1,8 @@
 package com.example.eixo.oficina.repository;
 
-public class OficinaRepository {
+import com.example.eixo.oficina.model.Oficina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OficinaRepository extends JpaRepository<Oficina, Long> {
+
 }
