2ad10350e32c6157cd864ea89f4aa40503bf2512
Barbara-fs <barbara.fds@outlook.com>
2026-09-20 22:46:27 -0300

    Iniciando CaixaService

--- eixo/src/main/java/com/example/eixo/fluxoCaixa/mapper/CaixaMapper.java
+++ eixo/src/main/java/com/example/eixo/fluxoCaixa/mapper/CaixaMapper.java
@@ -2,10 +2,12 @@ package com.example.eixo.fluxoCaixa.mapper;
 
 import com.example.eixo.fluxoCaixa.api.dto.CaixaRequest;
 import com.example.eixo.fluxoCaixa.model.Caixa;
+import com.example.eixo.fluxoCaixa.api.dto.CaixaResponse;
+
 
 public class CaixaMapper {
 
-    public Caixa TransformeEmEntidade (CaixaRequest caixaRequest){
+    public Caixa TransformeEmEntidade(CaixaRequest caixaRequest) {
         Caixa caixa = new Caixa();
         caixa.setDescricao(caixaRequest.descricao());
         caixa.setTipo(caixaRequest.tipo());
@@ -14,7 +16,22 @@ public class CaixaMapper {
         return caixa;
     }
 
-    public
+    public CaixaResponse toResponse(Caixa caixa) {
+        return new CaixaResponse(
+                caixa.getidluxoCaixa(),
+                caixa.getdescricao(),
+                caixa.gettipo(),
+                caixa.getvalor(),
+                caixa.getcategoria(),
+                caixa.getdataMovimentacao(),
+                caixa.getosVinculada(),
+                caixa.getpecaEstoque(),
+                caixa.getOficina
 
+    }
 
+        );
 }
+
+
+
--- eixo/src/main/java/com/example/eixo/fluxoCaixa/service/CaixaService.java
+++ eixo/src/main/java/com/example/eixo/fluxoCaixa/service/CaixaService.java
@@ -1,4 +1,9 @@
 package com.example.eixo.fluxoCaixa.service;
 
+@Service
 public class CaixaService {
+
+    private final CaixaRepository caixaRepository;
+    private final CaixaMapper caixaMapper;
+
 }
