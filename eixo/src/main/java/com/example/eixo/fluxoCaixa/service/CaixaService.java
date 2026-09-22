78a5937f21c124d723fa68c18a79733eebe297f7
Guest Blumenau <guest.bu@alunos.sc.senac.br>
2026-09-21 21:15:40 -0300

    Atualizações list

--- eixo/src/main/java/com/example/eixo/fluxoCaixa/service/CaixaService.java
+++ eixo/src/main/java/com/example/eixo/fluxoCaixa/service/CaixaService.java
@@ -1,4 +1,5 @@
 package com.example.eixo.fluxoCaixa.service;
+
 import com.example.eixo.excecao.excecoespersonalizadas.RecursoNaoEncontrado;
 import com.example.eixo.fluxoCaixa.model.Caixa;
 import com.example.eixo.fluxoCaixa.repository.CaixaRepository;
@@ -7,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 import com.example.eixo.fluxoCaixa.api.dto.CaixaResponse;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,17 +21,30 @@ public class CaixaService {
     private final CaixaMapper caixaMapper;
 
 
-Public Caixa  findById (Long Id) {
-    return CaixaRepository.findById(id)
-            .orElseThrow(() -> new RecursoNaoEncontrado("Caixa com id: " + id + " não encontrada"));
+    Public Caixa
+
+    findById(Long Id) {
+        return CaixaRepository.findById(id)
+                .orElseThrow(() -> new RecursoNaoEncontrado("Caixa com id: " + id + " não encontrada"));
+
+    }
+
+    Public CaixaResponse
+
+    encontrarfluxoCaixapeloId(Long Id) {
+        Caixa caixaEncontrada = findById(id);
+        CaixaResponse caixaResponse = caixaMapper.TrasnformarEmResposta(caixaEncontrada);
+        return caixaResponse;
+
+    }
+
+    pubic List
+
+    <CaixaResponse> ListarCaixa() {
+        List<Caixa> caixa = CaixaRepository.findAll();
+        List<CaixaResponse> caixaResponse = new ArrayList<>();
 
 }
-
-Public CaixaResponse encontrarfluxoCaixapeloId(Long Id){
-          Caixa caixaEncontrada = findById (id);
-          CaixaResponse caixaResponse = caixaMapper.TrasnformarEmResposta(caixaEncontrada);
-          return caixaResponse;
-
 
 
 }
