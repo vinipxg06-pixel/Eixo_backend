e882a43833f5ab3053f30a3fff9e8afb4b23788c
Guest Blumenau <guest.bu@alunos.sc.senac.br>
2026-09-22 20:51:58 -0300

    CaixaController finalizado

--- eixo/src/main/java/com/example/eixo/fluxoCaixa/api/CaixaController.java
+++ eixo/src/main/java/com/example/eixo/fluxoCaixa/api/CaixaController.java
@@ -1,4 +1,56 @@
 package com.example.eixo.fluxoCaixa.api;
 
+import com.example.eixo.fluxoCaixa.api.dto.CaixaRequest;
+import com.example.eixo.fluxoCaixa.api.dto.CaixaResponse;
+import com.example.eixo.fluxoCaixa.model.Caixa;
+import com.example.eixo.fluxoCaixa.service.CaixaService;
+import com.example.eixo.ordemservico.api.request.OrdemServicoResquest;
+import com.example.eixo.ordemservico.api.request.OrdemServicoStatus;
+import com.example.eixo.ordemservico.api.response.OrdemServicoResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("oficinas/{oficinaId}/caixa")
+@RequiredArgsConstructor
 public class CaixaController {
+
+    private final CaixaService caixaService;
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<CaixaResponse> buscarPorId(@PathVariable Long id) {
+        CaixaResponse caixa = caixaService.encontrarfluxoCaixapeloId(id);
+
+        return ResponseEntity.ok().body(caixa);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<CaixaResponse>> listarCaixa(@PathVariable Long oficinaId) {
+        return ResponseEntity.ok().body(caixaService.listarCaixa(oficinaId));
+    }
+
+    @PutMapping("/{caixaId}")
+    public ResponseEntity<CaixaResponse> atualizarCaixa(@Valid @RequestBody CaixaRequest caixaRequest, @PathVariable Long caixaId) {
+        return ResponseEntity.ok().body(caixaService.atualizarCaixa(caixaRequest, caixaId));
+    }
+
+
+    @DeleteMapping("/{caixaId}")
+    public void deletarCaixa(@PathVariable Long caixaId) {
+        caixaService.deletarCaixa(caixaId);
+    }
+
+    @PostMapping("/oficinas/{oficinaId}/caixa/")
+    public ResponseEntity<CaixaResponse> salvarCaixa(@Valid @RequestBody CaixaRequest caixaRequest) {
+        return ResponseEntity.ok().body(caixaService.salvarCaixa(caixaRequest));
+    }
 }
+
+
+
